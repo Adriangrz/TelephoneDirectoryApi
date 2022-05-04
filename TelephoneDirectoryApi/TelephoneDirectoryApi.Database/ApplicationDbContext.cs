10271132f004abaf6d354191f2d840681e8e4fd5
Adriangrz <adriang1412g@gmail.com>
2022-05-04 22:18:50 +0200

    Delete seed method

--- TelephoneDirectoryApi/TelephoneDirectoryApi.Database/ApplicationDbContext.cs
+++ TelephoneDirectoryApi/TelephoneDirectoryApi.Database/ApplicationDbContext.cs
@@ -24,18 +24,6 @@ namespace TelephoneDirectoryApi.Database
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-            GenerateTelephoneDirectory(modelBuilder);
-        }
-
-        private void GenerateTelephoneDirectory(ModelBuilder builder)
-        {
-            var telephoneDirectory1 = new EntryInTelephoneDirectory() { Id = Guid.NewGuid(), Name = "Antek", Surname = "Kowalski", City = "Warszawa", Street = "Wodna", StreetNumber = "35b", Number = 455433212 };
-            var telephoneDirectory2 = new EntryInTelephoneDirectory() { Id = Guid.NewGuid(), Name = "Marek", Surname = "Nowak", City = "Bielsko-Biała", Street = "Kryształowa", StreetNumber = "34a", Number = 123456788 };
-
-            builder.Entity<EntryInTelephoneDirectory>().HasData(
-                telephoneDirectory1,
-                telephoneDirectory2
-            );
         }
     }
 }
